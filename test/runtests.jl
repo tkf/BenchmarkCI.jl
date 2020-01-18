e4e1029ce6a34af101751aa5b0fe94ffd9d16345
Takafumi Arakaki <aka.tkf@gmail.com>
2020-01-17 21:47:14 -0800

    Add a smoke test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,13 @@ using BenchmarkCI
 using Test
 
 @testset "BenchmarkCI.jl" begin
-    # Write your own tests here.
+    mktempdir(prefix="BenchmarkCI_jl_test_") do dir
+        cd(dir) do
+            run(`git clone https://github.com/tkf/BenchmarkCIExample.jl`)
+            cd("BenchmarkCIExample.jl")
+            withenv("JULIA_LOAD_PATH" => "@:@stdlib") do
+                BenchmarkCI.runall()
+            end
+        end
+    end
 end
