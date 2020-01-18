a44765817a7f1781b42a01a96b382fcb52e28bb9
Takafumi Arakaki <aka.tkf@gmail.com>
2020-01-17 23:00:09 -0800

    Run tests via GitHub Actions (#1)

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,19 @@ using BenchmarkCI
 using Test
 
 @testset "BenchmarkCI.jl" begin
-    # Write your own tests here.
+    mktempdir(prefix = "BenchmarkCI_jl_test_") do dir
+        cd(dir) do
+            run(`git clone https://github.com/tkf/BenchmarkCIExample.jl`)
+            cd("BenchmarkCIExample.jl")
+
+            # Run a test without $GITHUB_TOKEN
+            withenv(
+                "JULIA_LOAD_PATH" => "@:@stdlib",
+                "CI" => "true",
+                "GITHUB_EVENT_PATH" => nothing,
+            ) do
+                BenchmarkCI.runall()
+            end
+        end
+    end
 end
