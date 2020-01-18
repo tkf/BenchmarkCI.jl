371bd9d95f4342e8236576c0960087e1549bcd6e
Takafumi Arakaki <aka.tkf@gmail.com>
2020-01-17 22:26:05 -0800

    Make test more robust

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,17 @@ using BenchmarkCI
 using Test
 
 @testset "BenchmarkCI.jl" begin
-    mktempdir(prefix="BenchmarkCI_jl_test_") do dir
+    mktempdir(prefix = "BenchmarkCI_jl_test_") do dir
         cd(dir) do
             run(`git clone https://github.com/tkf/BenchmarkCIExample.jl`)
             cd("BenchmarkCIExample.jl")
-            withenv("JULIA_LOAD_PATH" => "@:@stdlib") do
+
+            # Run a test without $GITHUB_TOKEN
+            withenv(
+                "JULIA_LOAD_PATH" => "@:@stdlib",
+                "CI" => "true",
+                "GITHUB_EVENT_PATH" => nothing,
+            ) do
                 BenchmarkCI.runall()
             end
         end
