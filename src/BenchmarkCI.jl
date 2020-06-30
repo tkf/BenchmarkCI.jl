6227d5b619039335a65d4f45f9b71fb8e9c6c6cb
Takafumi Arakaki <aka.tkf@gmail.com>
2020-06-29 23:58:09 -0700

    Fix status URL

--- src/BenchmarkCI.jl
+++ src/BenchmarkCI.jl
@@ -413,7 +413,7 @@ function pushresult(;
             "context" => "benchmarkci/pushresult",
             "description" => "Benchmarks complete!",
             "target_url" =>
-                "https://github.com/$(repo.full_name)/bolb/$branch/src/$datadir/result.md",
+                "https://github.com/$(repo.full_name)/blob/$branch/$datadir/result.md",
         )
         @info "Creating status" sha params = status_params
         GitHub.create_status(repo, sha; auth = auth, params = status_params)
