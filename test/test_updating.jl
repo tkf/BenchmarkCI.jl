797b9882df4727b4319576c2057e500dc2840ec4
Takafumi Arakaki <aka.tkf@gmail.com>
2020-07-02 16:18:54 -0700

    Retry git push (#64)
    * Retry git push
    
    * Setup dummy Git user in test

--- test/test_updating.jl
+++ test/test_updating.jl
@@ -3,13 +3,16 @@ module TestUpdating
 using Test
 using BenchmarkCI: GitUtils
 
+function setup_dummy_user()
+    run(`git config user.email "DUMMY@users.noreply.github.com"`)
+    run(`git config user.name DUMMY`)
+end
+
 function init_random_repo(dir, branch)
     mkpath(dir)
     cd(dir) do
         run(`git init .`)
-
-        run(`git config user.email "DUMMY@users.noreply.github.com"`)
-        run(`git config user.name DUMMY`)
+        setup_dummy_user()
 
         run(`git checkout -b $branch`)
         write("README.txt", "hello")
@@ -49,6 +52,43 @@ end
 @testset "prepare_ssh_command" begin
     mktempdir() do dir
         @test occursin(dir, GitUtils.prepare_ssh_command(dir, "dummy key"))
+    end
+end
+
+@testset "_push_with_retry" begin
+    mktempdir() do dir
+        branch = "somebranch"
+        origin = joinpath(dir, "origin")
+        init_random_repo(origin, branch)
+        url = "file://" * abspath(origin)
+
+        workdir = joinpath(dir, "workdir")
+        run(`git clone $url $workdir`)
+        cd(workdir) do
+            setup_dummy_user()
+        end
+
+        # Advance `origin`
+        cd(origin) do
+            run(`git checkout $branch`)
+            write("file-1", "hello")
+            run(`git add .`)
+            run(`git commit --message "Add file-1"`)
+            run(`git checkout -`)
+        end
+
+        cd(workdir) do
+            # Advance local `workdir`:
+            write("file-2", "hello")
+            run(`git add .`)
+            run(`git commit --message "Add file-2"`)
+
+            logs, = Test.collect_test_logs() do
+                GitUtils._push_with_retry(args -> `git $args`, branch)
+            end
+            all_messages = join((l.message for l in logs), "\n")
+            @test occursin("retrying", all_messages)
+        end
     end
 end
 
