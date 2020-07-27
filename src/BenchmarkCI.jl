9d5a1fce8cbe848f6db7f18be454a8af6c1e7d01
Takafumi Arakaki <aka.tkf@gmail.com>
2020-07-26 20:20:49 -0700

    More docstrings

--- src/BenchmarkCI.jl
+++ src/BenchmarkCI.jl
@@ -1,3 +1,11 @@
+"""
+    BenchmarkCI
+
+* [`BenchmarkCI.judge`](@ref): Run `benchmarkpkg` on `target` and `baseline`.
+* [`BenchmarkCI.postjudge`](@ref): Post judgement as comment.
+* [`BenchmarkCI.displayjudgement`](@ref): Print result of `BenchmarkCI.judge`.
+* [`BenchmarkCI.pushresult`](@ref): Push benchmark result to a repository.
+"""
 module BenchmarkCI
 
 import BenchmarkTools
